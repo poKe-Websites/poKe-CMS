b774859e287e891f884e4d940264d458551c3327
Thomas Wilbur <dodiaraculus@outlook.com>
2016-01-22 13:11:38 -0600

    [0.0.1]

--- admin/validate.php
+++ admin/validate.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(0);
 include("../config.php");
 $conn = new mysqli($hostname, $username, $password, $db_name);
-$query = mysqli_query($conn,"SELECT * FROM `Administrators` WHERE username = '" . addslashes($_POST["usernamep"]) . "' AND password='".  md5(addslashes$_POST["passwordp"])) . "'");
+$query = mysqli_query($conn,"SELECT * FROM `Administrators` WHERE username = '" . addslashes($_POST["usernamep"]) . "' AND password='".  md5(addslashes($_POST["passwordp"])) . "'");
 if(mysqli_num_rows($query) > 0){
 $_SESSION["username"] = $_POST["usernamep"];
 header("Location: dashboard.php");

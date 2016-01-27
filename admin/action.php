5066221d12cf4f4447191720dcf5078304395cc7
Thomas Wilbur <dodiaraculus@outlook.com>
2016-01-27 09:08:29 -0600

    Update action.php

--- admin/action.php
+++ admin/action.php
@@ -29,7 +29,7 @@ if (isset($_GET['password'])) {
     //die('update Administrators set username="' .addslashes($_POST["username"]) .'", password="' . md5(addslashes($_POST["password_ch"])) .'" where username=' . $_POST["username"]);
     $con = mysqli_connect($hostname, $username, $password, $db_name);
 
-    $sql = 'update Administrators set username="'.$conn->real_escape_string($_POST['username'])).'", password="'.substr(sha1($_POST['password_ch'], PASSWORD_BCRYPT), -10).'" where username="'.$_POST['username'].'"';
+    $sql = 'update Administrators set username="'.$conn->real_escape_string($_POST['username']).'", password="'.substr(sha1($_POST['password_ch'], PASSWORD_BCRYPT), -10).'" where username="'.$_POST['username'].'"';
 
     mysqli_query($con, $sql);
     mysqli_close($con);
@@ -37,7 +37,7 @@ if (isset($_GET['password'])) {
 }
 if (isset($_GET['deluser'])) {
     $conn = new mysqli($hostname, $username, $password, $db_name);
-    $sql = "DELETE FROM Administrators WHERE id='".$conn->real_escape_string($_GET['usr']))."'";
+    $sql = "DELETE FROM Administrators WHERE id='".$conn->real_escape_string($_GET['usr'])."'";
     $conn->query($sql);
     $conn->close();
     header('Location: users.php');
